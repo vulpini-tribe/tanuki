a026d2c36618d767466b124adfbaedbdf7ee677a
Alena Dzhukich <github@alena.red>
2023-12-05 21:50:12 +0600

    Create frontend demo page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,47 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+
 import './App.css';
+import logo from './logo.svg';
 
-const fetch_test_data = async () => {
-  const response = await window.fetch(`${process.env.REACT_APP_API_URL}/dishes`)
-  const body = await response.json();
+const queryClient = new QueryClient()
 
-  if (response.status !== 200) {
-    throw Error(body.message) 
-  }
+const Test = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['dishes'],
+    queryFn: () =>
+      fetch(`${process.env.REACT_APP_API_URL}/dishes`).then(
+        (res) => res.json(),
+      ),
+  });
 
-  return body;
+  if (isPending) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
+
+  console.log(data);
+
+  return (
+    <>
+      <header className="App-header">
+        <h1>Femboy Party</h1>
+      </header>
+
+      <main>
+        <img src={logo} className="App-logo" alt="logo" />
+      </main>
+    </>
+
+  )
 }
 
 const App = () => {
-  console.log('render sdf')
-  // useEffect(() => {
-  //   fetch_test_data()
-  //     .then(console.log)
-  //     .catch(console.error);
-  // }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Femboy Party</h1>
-
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Test/>
+      </div>
+    </QueryClientProvider>
   );
 }
 
