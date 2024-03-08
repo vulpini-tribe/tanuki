04998138ece0a7ef08e0f8780021b5f1b59d3f58
Alena Dzhukich <github@alena.red>
2024-03-08 12:26:57 +0500

    ...

--- tanuki-frontend/src/content-module/shared/shared-feed/shared-feed.tsx
+++ tanuki-frontend/src/content-module/shared/shared-feed/shared-feed.tsx
@@ -4,8 +4,7 @@ import { useLocation } from 'react-router-dom';
 import ROUTES from '@routes';
 
 import Root from './shared-feed.styles';
-import { CalendarIcon } from '@radix-ui/react-icons';
-import { ScrollArea, Grid, Heading, TextField } from '@radix-ui/themes';
+import { ScrollArea, Grid, Flex, Badge, TextField } from '@radix-ui/themes';
 
 const titles = {
 	[ROUTES.CONTENT.FEED]: 'Calendar Filters',
@@ -28,6 +27,14 @@ const SharedFeed = ({ children }) => {
 				<TextField.Input type="date" placeholder="From" variant="soft" color="gray" />
 				<TextField.Input type="date" placeholder="To" variant="soft" color="gray" />
 			</Grid>
+
+			<Flex p="2" gap="1" wrap="wrap">
+				<Badge>All time</Badge>
+				<Badge>Year</Badge>
+				<Badge>Half-year</Badge>
+				<Badge>Month</Badge>
+				<Badge>Fortnight</Badge>
+			</Flex>
 
 			<ScrollArea scrollbars="vertical" style={{ height: 'calc(100% - 48px)' }}>
 				{children}
