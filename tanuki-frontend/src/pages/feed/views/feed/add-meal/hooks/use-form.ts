e5d3f4b91f0d10a512a6d23009826b817693be64
Alena Dzhukich <github@alena.red>
2024-03-29 15:12:55 +0500

    feat: add feed entries

---
+++ tanuki-frontend/src/pages/feed/views/feed/add-meal/hooks/use-form.ts
@@ -0,0 +1,100 @@
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import yup from '@yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+type FormFields = {
+	name: string;
+	calories: number;
+	proteins?: number;
+	fats?: number;
+	carbs?: number;
+	weight: number;
+	icon?: string;
+	color?: string;
+	consumed_at?: string;
+};
+
+type FormKeys = keyof FormFields;
+
+const schema = yup.object().shape({
+	name: yup.string().required('Name is required'),
+	calories: yup.number().required('Calories is required').min(0, 'Calories must be greater than 0'),
+	proteins: yup.number(),
+	fats: yup.number(),
+	carbs: yup.number(),
+	weight: yup.number().required('Weight is required').min(0, 'Weight must be greater than 0'),
+	icon: yup.string(),
+	color: yup.string(),
+	consumed_at: yup.string()
+});
+
+const useFormHook = () => {
+	const { register, handleSubmit, formState, trigger, setError, clearErrors, getValues, setValue, resetField } =
+		useForm<FormFields>({
+			resolver: yupResolver(schema)
+		});
+
+	useEffect(() => {
+		clearErrors();
+	}, []);
+
+	const calcCalories = () => {
+		const { proteins, fats, carbs, weight } = getValues();
+		const calories = ((proteins || 0) * 4 + (fats || 0) * 9 + (carbs || 0) * 4) * (weight / 100);
+
+		if (calories) {
+			setValue('calories', calories);
+		}
+	};
+
+	return {
+		name: register('name'),
+		calories: register('calories', {
+			onChange: () => {
+				resetField('carbs');
+				resetField('fats');
+				resetField('proteins');
+			}
+		}),
+		proteins: register('proteins', {
+			onChange: () => {
+				calcCalories();
+			}
+		}),
+		fats: register('fats', {
+			onChange: () => {
+				calcCalories();
+			}
+		}),
+		carbs: register('carbs', {
+			onChange: () => {
+				calcCalories();
+			}
+		}),
+		weight: register('weight', {
+			onChange: () => {
+				calcCalories();
+			}
+		}),
+		icon: register('icon'),
+		color: register('color'),
+		consumed_at: register('consumed_at'),
+
+		submit: handleSubmit,
+		setError,
+		setValue,
+		getValues,
+		errors: formState.errors,
+		calcCalories,
+		revalidate: () => {
+			const fields = Object.keys(formState.dirtyFields);
+
+			if (fields.length) {
+				trigger(fields as FormKeys[]);
+			}
+		}
+	};
+};
+
+export default useFormHook;
--- tanuki-frontend/src/pages/feed/views/feed/feed.tsx
+++ tanuki-frontend/src/pages/feed/views/feed/feed.tsx
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useStoreMap } from 'effector-react';
+import { DateTime } from 'luxon';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import ROUTES, { createLinkWithQuery as createLink } from '@routes';
+
+import $feedStore, { dayDataSelector } from '@pages/feed/store';
+
+import { PlusIcon } from '@radix-ui/react-icons';
+import { Button, Box, Grid, Heading, ScrollArea } from '@radix-ui/themes';
+
+import FoodEntry from './food-entry';
+
+import type { MealEntryT } from '@pages/feed/types.d';
+import AddMeal from './add-meal';
 
 import Root from './feed.styles';
 
+const BRANCH_HOURS = 12;
+const LUNCH_HOURS = 14;
+const SNACK_HOURS = 16;
+const DINNER_HOURS = 20;
+
+const labels = {
+	breakfast: 'Breakfast',
+	branch: 'Branch',
+	lunch: 'Lunch',
+	snack: 'Snack',
+	dinner: 'Dinner'
+};
+
+type LabelsEnum = keyof typeof labels;
+
 const Feed = () => {
+	const test: Record<LabelsEnum, MealEntryT[]> = {
+		breakfast: [],
+		branch: [],
+		lunch: [],
+		snack: [],
+		dinner: []
+	};
+
+	const dayData = useStoreMap($feedStore, dayDataSelector);
+	const foodEntries = dayData.meals || [];
+
+	console.log(dayData);
+
+	const splittedFoodEntries = foodEntries.reduce((acc, food: MealEntryT) => {
+		const hour = (food.created_at as DateTime).get('hour');
+
+		if (hour >= DINNER_HOURS) {
+			acc.dinner.push(food);
+		} else if (hour >= SNACK_HOURS) {
+			acc.snack.push(food);
+		} else if (hour >= LUNCH_HOURS) {
+			acc.lunch.push(food);
+		} else if (hour >= BRANCH_HOURS) {
+			acc.branch.push(food);
+		} else {
+			acc.breakfast.push(food);
+		}
+
+		return acc;
+	}, test);
+
 	return (
 		<Root>
-			<div>Feed</div>
+			{Object.entries(splittedFoodEntries).map(([key, value]) => {
+				if (!value.length) return null;
+
+				return (
+					<Grid flow="row" rows="1fr" mt="5" key={key}>
+						<Heading size="4" mb="1">
+							{labels[key as LabelsEnum]}
+						</Heading>
+
+						{value.map((food) => {
+							return <FoodEntry key={food.id} {...food} />;
+						})}
+					</Grid>
+				);
+			})}
 		</Root>
 	);
 };
